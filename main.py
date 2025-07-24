ab191130a1fb5e52e0e1afcc180332e4e5388d0f
mdhabeebvulla <55741151+mdhabeebvulla@users.noreply.github.com>
2025-07-24 08:26:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+import joblib
+model = joblib.load("My_Iris")
+
+app = FastAPI()
+
+@app.get("/")
+def predict_iris(sl:float,sw:float,pl:float,pw:float):
+    result = model.predict([[sl,sw,pl,pw]]))
+    return {"Predicted Species is:":int(result[0])}
